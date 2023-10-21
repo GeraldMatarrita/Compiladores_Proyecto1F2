ff0b387dcf9a2ec9ad02112a130388a2cb033330
GeraldMatarrita <geramatarr@gmail.com>
2023-10-20 23:06:59 -0600

    Parser

--- src/main/java/SLRParser.java
+++ src/main/java/SLRParser.java
@@ -2,33 +2,33 @@ import java.util.Stack;
 
 public class SLRParser {
     private String[][] parsingTable = {
-            //   "(", ")", "*", "+", "-", "/", ";", "=", "i", "n", "$", "E", "P", "T",
-            {"s1", null, null, null, null, null, null, null, "s5", "s6", null, "2", "3", "4"},
-            {"s1", null, null, null, null, null, null, null, "s5", "s6", null, "7", null, "4"},
-            {null, null, null, "s8", "s9", null, "s10", null, null, null, null, null, null, null},
-            {null, null, null, null, null, null, "s11", null, null, null, "acc", null, null, null},
-            {null, "r3", "s12", "s13", "s14", "s15", "r3", null, null, null, null, null, null, null},
-            {null, "r9", "r9", "r9", "r9", "r9", "r9", "s16", null, null, null, null, null, null},
-            {null, "r10", "r10", "r10", "r10", "r10", "r10", null, null, null, null, null, null, null},
-            {null, "s17", null, "s8", "s9", null, null, null, null, null, null, null, null, null},
-            {"s1", null, null, null, null, null, null, null, "s5", "s6", null, "18", null, "4"},
-            {"s1", null, null, null, null, null, null, null, "s5", "s6", null, "19", null, "4"},
-            {null, null, null, null, null, null, "r1", null, null, null, "r1", null, null, null},
-            {null, null, null, null, null, null, "r2", null, null, null, "r2", null, null, null},
-            {"s1", null, null, null, null, null, null, null, "s21", "s6", null, null, "20", null},
-            {"s1", null, null, null, null, null, null, null, "s21", "s6", null, null, "22", null},
-            {"s1", null, null, null, null, null, null, null, "s21", "s6", null, null, "23", null},
-            {"s1", null, null, null, null, null, null, null, "s21", "s6", null, null, "24", null},
-            {"s1", null, null, null, null, null, null, null, "s5", "s6", null, "25", null, "4"},
-            {null, "r11", "r11", "r11", "r11", "r11", "r11", null, null, null, null, null, null, null},
-            {null, "r4", null, "s8", "s9", null, "r4", null, null, null, null, null, null, null},
-            {null, "r5", null, "s8", "s9", null, "r5", null, null, null, null, null, null, null},
-            {null, "r12", "s12", "r12", "r12", "s15", "r12", null, null, null, null, null, null, null},
-            {null, "r9", "r9", "r9", "r9", "r9", "r9", null, null, null, null, null, null, null},
-            {null, "r6", "s12", "r6", "r6", "s15", "r6", null, null, null, null, null, null, null},
-            {null, "r7", "s12", "r7", "r7", "s15", "r7", null, null, null, null, null, null, null},
-            {null, "r13", "s12", "r13", "r13", "s15", "r13", null, null, null, null, null, null, null},
-            {null, "r8", null, "s8", "s9", null, "r8", null, null, null, null, null, null, null},
+        //     (      )      *      +      -      /      ;      =      i      n      $      E      P     T
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s5",  "s6",  null,  "2",   "3",   "4"},
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s5",  "s6",  null,  "7",   null,  "4"},
+            {null,  null,  null,  "s8",  "s9",  null,  "s10", null,  null,  null,  null,  null,  null,  null},
+            {null,  null,  null,  null,  null,  null,  "s11", null,  null,  null,  "acc", null,  null,  null},
+            {null,  "r3",  "s12", "s13", "s14", "s15", "r3",  null,  null,  null,  null,  null,  null,  null},
+            {null,  "r9",  "r9",  "r9",  "r9",  "r9",  "r9",  "s16", null,  null,  null,  null,  null,  null},
+            {null,  "r10", "r10", "r10", "r10", "r10", "r10", null,  null,  null,  null,  null,  null,  null},
+            {null,  "s17", null,  "s8",  "s9",  null,  null,  null,  null,  null,  null,  null,  null,  null},
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s5",  "s6",  null,  "18",  null,  "4"},
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s5",  "s6",  null,  "19",  null,  "4"},
+            {null,  null,  null,  null,  null,  null,  "r1",  null,  null,  null,  "r1",  null,  null,  null},
+            {null,  null,  null,  null,  null,  null,  "r2",  null,  null,  null,  "r2",  null,  null,  null},
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s21", "s6",  null,  null,  "20",  "20"},
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s21", "s6",  null,  null,  "22",  "22"},
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s21", "s6",  null,  null,  "23",  "23"},
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s21", "s6",  null,  null,  "24",  "24"},
+            {"s1",  null,  null,  null,  null,  null,  null,  null,  "s5",  "s6",  null,  "25",  null,  "4"},
+            {null,  "r11", "r11", "r11", "r11", "r11", "r11", null,  null,  null,  null,  null,  null,  null},
+            {null,  "r4",  null,  "s8",  "s9",  null,  "r4",  null,  null,  null,  null,  null,  null,  null},
+            {null,  "r5",  null,  "s8",  "s9",  null,  "r5",  null,  null,  null,  null,  null,  null,  null},
+            {null,  "r12", "s12", "r12", "r12", "s15", "r12", null,  null,  null,  null,  null,  null,  null},
+            {null,  "r9",  "r9",  "r9",  "r9",  "r9",  "r9",  null,  null,  null,  null,  null,  null,  null},
+            {null,  "r6",  "s12", "r6",  "r6",  "s15", "r6",  null,  null,  null,  null,  null,  null,  null},
+            {null,  "r7",  "s12", "r7",  "r7",  "s15", "r7",  null,  null,  null,  null,  null,  null,  null},
+            {null,  "r13", "s12", "r13", "r13", "s15", "r13", null,  null,  null,  null,  null,  null,  null},
+            {null,  "r8",  null,  "s8",  "s9",  null,  "r8",  null,  null,  null,  null,  null,  null,  null},
     };
 
     private String[] inputTokens;
@@ -73,13 +73,9 @@ public class SLRParser {
                     }
                 }
                 String reducedSymbol = getReducedSymbol(productionRule);
-                if (reducedSymbol.equals("P")){
-                    stack.push("3");
-                } else {
-                    String newState = parsingTable[Integer.parseInt(stack.peek())][getNonTerminalIndex(reducedSymbol)];
-                    stack.push(reducedSymbol);
-                    stack.push(newState);
-                }
+                String newState = parsingTable[Integer.parseInt(stack.peek())][getNonTerminalIndex(reducedSymbol)];
+                stack.push(reducedSymbol);
+                stack.push(newState);
             }
         }
     }
@@ -111,7 +107,7 @@ public class SLRParser {
     private int getProductionLength(int productionRule) {
         // Define the length of each production rule
         // Modify this based on your actual grammar
-        int[] productionLengths = {0, 1, 1, 1, 3, 3, 3, 3, 3, 3, 1, 1, 1, 3, 3};
+        int[] productionLengths = {0, 2, 2, 1, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3};
         if (productionRule >= 0 && productionRule < productionLengths.length) {
             return productionLengths[productionRule];
         }
@@ -121,7 +117,7 @@ public class SLRParser {
     private String getReducedSymbol(int productionRule) {
         // Define the left-hand side symbol of each production rule
         // Modify this based on your actual grammar
-        String[] reducedSymbols = {"P'", "P", "P", "E", "E", "E", "E", "E", "E", "E", "T", "T", "T", "T", "T"};
+        String[] reducedSymbols = {"P'", "P", "P", "E", "E", "E", "E", "E", "E", "T", "T", "T", "T"};
         if (productionRule >= 0 && productionRule < reducedSymbols.length) {
             return reducedSymbols[productionRule];
         }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        String[] inputTokens = {"i", "=", "i", "-", "(", "i", ")", ";", "$"};
+
+        String[] inputTokens = {"i", "=", "n", "*", "(", "i", "+", "n", ")", "$"};
         SLRParser parser = new SLRParser(inputTokens);
         parser.parse();
     }
